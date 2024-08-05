056724dd00e34e10c40f75fd48530dbe5489aac4
JoshuaWoods <nordev@protonmail.com>
2024-08-04 21:09:45 -0500

    setup

--- src/data_cli.cr
+++ src/data_cli.cr
@@ -2,5 +2,25 @@
 module DataCli
   VERSION = "0.1.0"
 
-  # TODO: Put your code here
-end
+  #Module and Version Definition
+  class CLI 
+    def  initialize
+       @data_folder = "./data"
+    end
+   
+
+  #Class Definition
+   def run  
+   
+  
+  
+  
+  end
+  
+  #Initialization
+  
+  #Public Methods
+  
+  #Private Helper Methods
+  
+  #Main Entry Point
